ccc30ab9900789cc148a1fc6131af17e68541144
Pablo <pmahuzieru@gmail.com>
2026-05-01 22:50:35 -0400

    Get actual route to random point and back

--- script.js
+++ script.js
@@ -25,35 +25,72 @@ navigator.geolocation.getCurrentPosition(
     }
 );
 
-// Generate random route (basic v1)
-function generateRoute() {
+function getRandomPoint(lat, lng, distanceKm) {
+    const R = 6371;  // Earth radius in km
+
+    const bearing = Math.random() * 2 * Math.PI;
+
+    const d = distanceKm / R;
+
+    const lat1 = (lat * Math.PI) / 180;
+    const lng1 = (lng * Math.PI) / 180;
+
+    const lat2 = Math.asin(
+        Math.sin(lat1) * Math.cos(d) +
+            Math.cos(lat1) * Math.sin(d) * Math.cos(bearing)
+    );
+
+    const lng2 =
+        lng1 +
+        Math.atan2(
+            Math.sin(bearing) * Math.sin(d) * Math.cos(lat1),
+            Math.cos(d) - Math.sin(lat1) * Math.sin(lat2)
+        );
+
+    return [ (lat2 * 180) / Math.PI, (lng2 * 180) / Math.PI];
+}
+
+// v2: use ORS walking route API
+async function generateRoute() {
     if (!userLocation) return;
 
     const distanceKm = parseFloat(document.getElementById("distance").value) || 3;
 
     const [lat, lng] = userLocation;
 
-    // simple random offsets
-    const offset = distanceKm / 100;
+    const destination = getRandomPoint(lat, lng, distanceKm / 2);
 
-    const pointA = [
-        lat + (Math.random() - 0.5) * offset,
-        lng + (Math.random() - 0.5) * offset,
-    ];
+    const apiKey = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjA1NmU5MmFkZmJjYjQ2MTE5ZjIyNjdiYjNjZTY0YWEyIiwiaCI6Im11cm11cjY0In0=";
 
-    const pointB = [
-        lat + (Math.random() - 0.5) * offset,
-        lng + (Math.random() - 0.5) * offset,
-    ];
+    const response = await fetch(
+        "https://api.openrouteservice.org/v2/directions/foot-walking",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: apiKey,
+            },
+            body: JSON.stringify({
+                coordinates: [
+                    [lng, lat],
+                    [destination[1], destination[0]],
+                    [lng, lat],
+                ],
+            }),
+        }
+    );
 
-    const route = [userLocation, pointA, pointB, userLocation];
+    const data = await response.json()
 
-    // remove previous route
+    const encoded = data.routes[0].geometry;
+    const decoded = polyline.decode(encoded);
+    const coords = decoded;
+
     if (routeLayer) {
         map.removeLayer(routeLayer);
     }
 
-    routeLayer = L.polyline(route, { color: "#4ea1ff"}).addTo(map);
+    routeLayer = L.polyline(coords, { color: "#4ea1ff" }).addTo(map);
 
     map.fitBounds(routeLayer.getBounds());
 }
